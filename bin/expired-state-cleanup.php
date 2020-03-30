47aaf741fc671c8bbc15d16705728f9197fb469c
Rob Skilling <rob@dxw.com>
2020-03-30 12:47:21 +0100

    Take state_time_limit value from admin settings

--- bin/expired-state-cleanup.php
+++ bin/expired-state-cleanup.php
@@ -15,9 +15,13 @@ define('WP_INSTALLING', true);
 include_once $argv[1];
 // End WP boilerplate
 
+$open_id_settings = get_option('openid_connect_generic_settings');
 $expiration_time_in_seconds = 180;
+if(isset($open_id_settings['state_time_limit']) && (int)$open_id_settings['state_time_limit'] > 0) {
+    $expiration_time_in_seconds = (int)$open_id_settings['state_time_limit'];
+}
 
-error_log('OpenID Connect Generic Client: Starting expired state cleanup');
+error_log('OpenID Connect Generic Client: Starting expired state cleanup, cleaning tokens older than ' . $expiration_time_in_seconds . ' seconds');
 
 $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}options WHERE option_name LIKE \"openid-connect-generic-state-%\"", OBJECT );
 
